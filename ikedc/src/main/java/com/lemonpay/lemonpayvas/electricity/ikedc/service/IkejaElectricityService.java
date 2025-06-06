5a181ee58fcd0008c63ea9306446c530e63aa51f
ugochukw.omeje <ugochukwu.omeje@etranzact.com>
2025-06-06 15:23:10 +0100

    create ikeja electricity node

---
+++ ikedc/src/main/java/com/lemonpay/lemonpayvas/electricity/ikedc/service/IkejaElectricityService.java
@@ -0,0 +1,27 @@
+package com.lemonpay.lemonpayvas.electricity.ikedc.service;
+
+import com.lemonpay.lemonpayvas.electricity.phcnnode.PHCNNode;
+import com.lemonpay.lemonpayvas.electricity.phcnnode.dto.*;
+
+public class IkejaElectricityService extends PHCNNode {
+    @Override
+    public ElectricityQueryResponse query(ElectricityQueryRequest electricityQueryRequest) {
+
+        return null;
+    }
+
+    @Override
+    public ElectricityProcessResponse process(ElectricityProcessRequest electricityProcessRequest) {
+        return null;
+    }
+
+    @Override
+    public ElectricityProcessResponse reQuery(ElectricityReQueryRequest electricityReQueryRequest) {
+        return null;
+    }
+
+    @Override
+    public PingResponse ping(ElectricityQueryRequest electricityQueryRequest) {
+        return null;
+    }
+}
